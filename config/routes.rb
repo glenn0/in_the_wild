8391b75d688cc867182aa9e4f4ad5a306148bdff
glenn0 <glenn.osullivan@gmail.com>
2014-01-21 02:06:27 +1100

    Cleaning up unused test page/controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 InTheWild::Application.routes.draw do
   root "pages#home"    
   get "home", to: "pages#home", as: "home"
-  get "inside", to: "pages#inside", as: "inside"
   
     
   devise_for :users
