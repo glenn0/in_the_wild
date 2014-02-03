22620934018222f3ec0f6fe6104c1ea0083e27a1
glenn0 <glenn.osullivan@gmail.com>
2014-02-03 13:49:39 +1100

    Minor: change thumbs to stars for votes in UI.

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,4 +1,6 @@
 class Vote < ActiveRecord::Base
   belongs_to :creator, class_name: 'User', foreign_key: 'user_id'
   belongs_to :voteable, polymorphic: true
+
+  # TODO validate so that only each project can only have one active vote per user
 end
